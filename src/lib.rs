7cc9d9ebca0368c40cd47f7c9a82533ffe6b9694
Markus Kasten <github@markuskasten.eu>
2024-08-11 17:13:38 +0200

    feat: add helper function for calculating offset register value

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,7 @@ pub enum Error<E> {
     InvalidDate,
 }
 
+#[derive(Debug, Clone, Copy)]
 pub enum OffsetMode {
     /// Correction made every 4 hours, 2.170ppm/step
     Normal,
@@ -33,6 +34,20 @@ impl OffsetMode {
             OffsetMode::Fast => 20345,
         }
     }
+}
+
+/// Helper function to calculate the offset value for a given offset in ppb.
+///
+/// This essentially maps Table 24 from the datasheet.
+pub fn offset_value_for_ppb_offset(offset_ppb: i32, offset_mode: OffsetMode) -> i8 {
+    let tenthppb_per_pulse: i64 = match offset_mode {
+        OffsetMode::Normal => 21700,
+        OffsetMode::Fast => 20345,
+    };
+
+    (((offset_ppb as i64) * 10 + (offset_ppb.signum() as i64 * tenthppb_per_pulse / 2))
+        / tenthppb_per_pulse)
+        .clamp(i8::MIN as i64, i8::MAX as i64) as i8
 }
 
 pub struct Pcf85263a<I> {
@@ -58,5 +73,38 @@ where
 {
     pub fn new_with_i2c(i2c: I2C) -> Self {
         Self::new(I2cInterface::new(i2c, DEFAULT_ADDRESS))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_offsets() {
+        let table = [
+            // a selection of test cases to make sure rounding is
+            // working correctly
+            (0, 0, OffsetMode::Normal),
+            (0, 0, OffsetMode::Fast),
+            (2, 5_000, OffsetMode::Normal),
+            (0, 600, OffsetMode::Fast),
+            (1, 1_100, OffsetMode::Fast),
+            (0, -500, OffsetMode::Normal),
+            (-1, -2_000, OffsetMode::Normal),
+            (126, 256_000, OffsetMode::Fast),
+            (127, 300_000, OffsetMode::Normal),
+            (-127, -275_600, OffsetMode::Normal),
+            (-128, -300_000, OffsetMode::Normal),
+        ];
+
+        for test in table {
+            let offset = offset_value_for_ppb_offset(test.1, test.2);
+            assert_eq!(
+                offset, test.0,
+                "Offset in ppb: {}, offset: {}, expected: {}, mode: {:?}",
+                test.1, offset, test.0, test.2
+            );
+        }
     }
 }
